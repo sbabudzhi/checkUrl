7429170a6356953c02a29b02c5321124856bb6e9
babudzhi <babudhi@lanit.ru>
2018-04-17 16:56:44 +0300

    ee

---
+++ src/main/java/ru/babudzhi/HelloWorld.java
@@ -0,0 +1,12 @@
+package main.java.ru.babudzhi;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, new branch");
+        System.out.println("Недурно, сэр, недурно.");
+        System.out.println("1111");
+
+        System.out.println("aaaa");
+
+    }
+}
